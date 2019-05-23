a2c701730a1885fdb6e75de8907976156b44ef92
Alexandr <alexandrmail32@gmail.com>
2019-05-23 14:59:24 +0300

    add disable class

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const arrowLeft = document.querySelector('.left');
+const arrowRight = document.querySelector('.right');
 const elemToScroll = document.querySelector('.scroll');
 const imgContainer = document.querySelector('.img_wrapper');
 const imgAmount = imgContainer.querySelectorAll('.img');
@@ -10,16 +12,20 @@ document.querySelector('.wrapper').addEventListener('click', scroll(elemToScroll
 function scroll(elem) {
   return function (event) {
     if (event.target.classList.contains('left')){
+      arrowRight.classList.remove('disabled');
       left = x - imgContainer.offsetWidth;
-      if (left < -910) {
+      if (left <= -910) {
         left = -910;
+        arrowLeft.classList.add('disabled');
       }
       elem.style.left = left + "px";
       x = left;
     } else if (event.target.classList.contains('right')) {
+      arrowLeft.classList.remove('disabled');
       left = x + imgContainer.offsetWidth;
       if (left >= 0) {
         left = 0;
+        arrowRight.classList.add('disabled');
       }
       elem.style.left = left + "px";
       x = left;
